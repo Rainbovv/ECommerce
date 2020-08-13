488df887be8d92ef605194ba599057cf1df76dcb
Rainbovv <turcanu.radu17@gmail.com>
2020-08-13 04:26:56 +0300

    done

--- src/main/java/domain/repos/DataRepo.java
+++ src/main/java/domain/repos/DataRepo.java
@@ -2,34 +2,43 @@ package domain.repos;
 
 import domain.interfaces.DataRepository;
 import java.io.*;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
 
+@SuppressWarnings("unchecked")
 public class DataRepo implements DataRepository {
 
 	private DataRepo() {}
 
+	private URL getFilePathByClassType(Class<?> classType) {
+
+		String fileName;
+
+		if (classType == ArrayList.class)
+			fileName = "currencies";
+
+		else if (classType == Date.class)
+			fileName = "date_of_update";
+
+		else {
+			fileName = classType.toString().toLowerCase();
+			fileName = fileName.substring(fileName.lastIndexOf(".") + 1);
+		}
+
+		return this.getClass().getResource("/" + fileName + ".bin");
+
+	}
+
 	@Override
 	public <T> void save(T entity) {
 
-		T object = entity;
-		
-		String fileName = entity.getClass().toString().toLowerCase();
-		fileName = fileName.substring(fileName.lastIndexOf(".") + 1);
-
-		if (entity.getClass() == ArrayList.class)
-			fileName = "currencies";
-
-		if (entity.getClass() == Date.class)
-			fileName = "date_of_update";
-
 		try {
-			File file = new File(this.getClass().getResource("/" + fileName + ".bin").toURI());
+
+			File file = new File(getFilePathByClassType(entity.getClass()).toURI());
 			ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(file));
 
-			out.writeObject(object);
+			out.writeObject(entity);
 			out.close();
 
 		} catch (Exception e) {
@@ -40,31 +49,22 @@ public class DataRepo implements DataRepository {
 	@Override
 	public <T> T load(Class<?> classType) {
 
-		T t = null;
+		T entity = null;
 
-		String fileName = classType.toString().toLowerCase();
-		fileName = fileName.substring(fileName.lastIndexOf(".") + 1);
-
-		if (classType == ArrayList.class)
-			fileName = "currencies";
-
-		if (classType == Date.class)
-			fileName = "date_of_update";
-		
 		try {
-			File file = new File(this.getClass().getResource("/" + fileName + ".bin").toURI());
+			File file = new File(getFilePathByClassType(classType).toURI());
 			ObjectInputStream in = new ObjectInputStream(new FileInputStream(file));
 
-			t = (T) in.readObject();
+			entity = (T)in.readObject();
 			in.close();
 
-		} catch (Exception e) {
-//			e.printStackTrace();
+		}catch (Exception e) {
+			e.printStackTrace();
 		}
 		
-		if (t == null) System.err.println("The file is empty!");
+		if (entity == null) System.err.println("The file is empty!");
 
-		return t;
+		return entity;
 	}
 
 	private static class SingletonHolder {
--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -10,47 +10,30 @@ public class Application {
     public static void main(String[] args) {
 
 
-        Product product = (Product) ProductFactory.getInstance().getProduct("Плюшевый Шмель", 15.99f, 125, 156, 10, 25, "China", "Concrete Product");
-
-
-
-
-        Money money = MoneyProvider.getInstance().getMoney(1000);
-
-        DataRepo.getInstance().save(money);
-
-        money = DataRepo.getInstance().load(Money.class);
-
-        System.out.println(money);
-
+        Product product = (Product) ProductFactory.getInstance()
+                .getProduct("Плюшевый Шмель", 15.99f, 125, 156,
+                        10, 25, "China", "Concrete Product");
         DataRepo.getInstance().save(product);
         product = DataRepo.getInstance().load(product.getClass());
         System.out.println(product);
 
-        Product fakeProduct = (Product)ProductFactory.getInstance()
-                                                     .getFakeProduct();
 
 
-//
+        Money money = MoneyProvider.getInstance().getMoney(1000);
+        DataRepo.getInstance().save(money);
+        money = DataRepo.getInstance().load(Money.class);
+        System.out.println(money);
+
+
+
+        Product fakeProduct = (Product)ProductFactory.getInstance().getFakeProduct();
         System.out.println(fakeProduct);
-
 
         fakeProduct.setPrice(fakeProduct.getPrice().toCurrency("RON"));
         System.out.println(fakeProduct);
-//        System.out.println(fakeProduct.getPrice().getCurrency().getRate());
-//        System.out.println(fakeProduct);
-//
-        fakeProduct.setPrice(fakeProduct.getPrice().toCurrency("MDL"));
+
+        fakeProduct.setPrice(fakeProduct.getPrice().toCurrency("ML"));
         System.out.println(fakeProduct);
-
-
-//        Cart.getInstance().add(product);
-//        Cart.getInstance().add(fakeProduct);
-
-//        System.out.println(Cart.getInstance().getTotal());
-//        System.out.println(Cart.getInstance().getTotal("RUB"));
-
-//        Cart.getInstance().findAll().forEach(System.out::println);
 
     }
 }
